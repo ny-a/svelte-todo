0904f381c018abcdcc5bf31b7a801e256126c81a
ny-a <58758248+ny-a@users.noreply.github.com>
2020-01-06 11:15:12 +0900

    Implement Todo app

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,52 @@
 <script>
-	export let name;
+    let todos = [
+        { id: 0, done: false, text: "Sample Todo" },
+    ];
+    let text = "";
+
+    $: nextId = Math.max(...todos.map(i => i.id)) + 1;
+
+    function addTodo() {
+        const newTodo = { id: nextId, done: false, text: text };
+        todos = [...todos, newTodo];
+        text = "";
+    }
+
+    function toggleDoneStatus(id) {
+        todos = todos.map(i => {
+            if (i.id === id) {
+                return {
+                    id: i.id,
+                    done: !i.done,
+                    text: i.text,
+                }
+            } else {
+                return i
+            }
+        })
+    }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>TODO List</h1>
+
+    <input bind:value={text} />
+    <button on:click={addTodo}>Add</button>
+
+    <div class="todoContainer">
+        <ul>
+            {#each todos as todo}
+                <li on:click={toggleDoneStatus(todo.id)}>
+                    {#if todo.done}
+                        <s>{todo.text}</s>
+                    {:else}
+                        {todo.text}
+                    {/if}
+                </li>
+            {/each}
+        </ul>
+    </div>
+
 </main>
 
 <style>
@@ -21,6 +63,15 @@
 		font-size: 4em;
 		font-weight: 100;
 	}
+
+    .todoContainer {
+        margin: 0 auto;
+        max-width: 400px;
+    }
+
+    ul {
+        text-align: left;
+    }
 
 	@media (min-width: 640px) {
 		main {
